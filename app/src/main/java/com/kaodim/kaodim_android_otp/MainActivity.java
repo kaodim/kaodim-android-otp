3f4436f4c00e3348ce1e93661d3250429224832d
Dinuka Jayasuriya <dinukapps@gmail.com>
2018-07-16 17:13:20 +0800

    Initial commit

---
+++ app/src/main/java/com/kaodim/kaodim_android_otp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kaodim.kaodim_android_otp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
